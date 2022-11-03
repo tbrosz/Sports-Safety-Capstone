2ee446a36f4bb4a8846458a07244eefb780c203e
nrrai001 <113865152+nrrai001@users.noreply.github.com>
2022-11-03 02:17:10 -0400

    Update main.py

--- EmailAlert/main.py
+++ EmailAlert/main.py
@@ -7,7 +7,7 @@ def main():
     mailer.setSubject("This is demo")
     mailer.setMessage("This is a test email generated from a python program!")
     mailer.setAttachement("demo.pdf")
-    mailer.send("raid972020@gmail.com")
+    mailer.send("raidraid972020@gmail.com")
     print("Email send successfully!")
 
 main()
